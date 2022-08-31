722dd9076fe42d8ab0893c3de3d261d0df96a2e7
Dan Lopez <dan@midpointmedia.com>
2022-08-31 10:49:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,7 @@ my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_data_rows.index)
+
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
